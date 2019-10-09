0dd09dc7318b0d7905d1f6735b11c691b09195dc
Gustavo costa <gustavocosta@bemol.com.br>
2019-10-09 11:25:12 -0400

    porta

--- common/environment.ts
+++ common/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     server: {
-        port: process.env.SERVER_PORT || 3000
+        port: process.env.PORT || 3000
     },
     db: {
         url: process.env.DB_URL || 'mongodb+srv://user_admin:adminpasswd@clusterteste-9y6hc.mongodb.net/innovations?retryWrites=true&w=majority'

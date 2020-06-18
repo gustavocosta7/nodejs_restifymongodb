3a69151e428374cb081b1aabf526c99562877452
Gustavo Costa <gustavolegal7@gmail.com>
2020-06-18 03:24:19 -0400

    Update environment.ts

--- common/environment.ts
+++ common/environment.ts
@@ -4,6 +4,5 @@ export const environment = {
     },
     db: {
         url: process.env.DB_URL ||  'mongodb://localhost:27017/dbInovacao'
-        // url: process.env.DB_URL ||  'mongodb://db-chatbot:culzzeZzfGYO8BEqnXmlt0P7OTh3WP07OkwzR5ok1XLCyXp4AJinkC6jpfqExncdUfMASbJrIrdywwfPlM1JZg==@db-chatbot.documents.azure.com:10255/inovacao_db_QAS?ssl=true'
     }
 }
